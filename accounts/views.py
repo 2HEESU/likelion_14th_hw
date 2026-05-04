2503e8e1f8f46c3ddbaf35c399a8f4318cd16869
2HEESU <sunf05121@gmail.com>
2026-05-05 02:30:40 +0900

    Feat: 4주차 사용자 식별과 인증 기능 구현하기

---
+++ accounts/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+from .models import Profile
+
+# Create your views here.
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('main:postpage')
+        else:
+            return render(request, 'accounts/login.html')
+        
+    elif request.method == 'GET':
+        return render(request, 'accounts/login.html')
+    
+def logout(request):
+    auth.logout(request)
+    return redirect('main:postpage')
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+
+        if User.objects.filter(username=username).exists():
+            # 이미 있으면 메시지와 함께 가입 페이지로 다시 돌려보냄
+            return render(request, 'accounts/signup.html', {'error': '이미 있는 아이디입니다!'})
+        
+        if request.POST['password'] == request.POST['confirm']:
+            newuser = User.objects.create_user(
+                username=request.POST['username'],
+                password=request.POST['password'],
+            )
+
+            nickname = request.POST['nickname']
+            major = request.POST['major']
+            age = request.POST['age']
+            profile_image = request.FILES.get('profile_image')
+
+            profile = Profile(
+                user=newuser,
+                nickname=nickname,
+                major=major,
+                age=age,
+                profile_image=profile_image,
+            )
+            profile.save()
+            
+            auth.login(request, newuser)
+            return redirect('main:postpage')
+        
+    return render(request, 'accounts/signup.html')
